07a1153e56e1d893b0fbe40b8863297ced5187c1
Kitson Kelly <me@kitsonkelly.com>
2021-12-22 08:00:30 +1100

    fmt

--- src/ast.rs
+++ src/ast.rs
@@ -429,31 +429,34 @@ function b(c) {
     assert!(result.is_ok());
     let parsed_source = result.unwrap();
     let dependencies = analyze_jsdoc_imports(&parsed_source);
-    assert_eq!(json!(dependencies), json!([
-      [
-        "./a.js",
-        {
-          "start": 62,
-          "end": 68,
-          "ctxt": 0
-        }
-      ],
-      [
-        "./b.js",
-        {
-          "start": 146,
-          "end": 152,
-          "ctxt": 0
-        }
-      ],
-      [
-        "./d.js",
-        {
-          "start": 179,
-          "end": 185,
-          "ctxt": 0
-        }
-      ]
-    ]));
+    assert_eq!(
+      json!(dependencies),
+      json!([
+        [
+          "./a.js",
+          {
+            "start": 62,
+            "end": 68,
+            "ctxt": 0
+          }
+        ],
+        [
+          "./b.js",
+          {
+            "start": 146,
+            "end": 152,
+            "ctxt": 0
+          }
+        ],
+        [
+          "./d.js",
+          {
+            "start": 179,
+            "end": 185,
+            "ctxt": 0
+          }
+        ]
+      ])
+    );
   }
 }
