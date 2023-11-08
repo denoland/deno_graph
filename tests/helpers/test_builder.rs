8b6829c1a83c3e9ec3dcddb1470da3ae223d6dbf
David Sherret <dsherret@gmail.com>
2023-11-08 12:44:32 -0500

    Add symbol delcs in output

--- tests/helpers/test_builder.rs
+++ tests/helpers/test_builder.rs
@@ -296,6 +296,28 @@ impl TestBuilder {
               ModuleInfoRef::Json(m) => format!("{:#?}", m),
             }
           );
+          output_text.push_str(&module_output_text);
+
+          let mut symbol_deps_text = String::new();
+          for symbol in module.symbols() {
+            for decl in symbol.decls() {
+              if let Some((node, source)) = decl.maybe_node_and_source() {
+                let deps = node.deps();
+                if !deps.is_empty() {
+                  symbol_deps_text.push_str(&format!(
+                    "{:?}:{:?} {:?}\n",
+                    symbol.symbol_id(),
+                    decl.range.as_byte_range(source.text_info().range().start),
+                    deps
+                  ));
+                }
+              }
+            }
+          }
+          if !symbol_deps_text.is_empty() {
+            output_text
+              .push_str(&format!("== symbol decls ==\n{}\n", symbol_deps_text));
+          }
 
           // analyze the module graph for any problems
           let diagnostics = check_fatal_diagnostics(module);
@@ -307,8 +329,6 @@ impl TestBuilder {
             eprintln!("== {} == \n\n{}", specifier, diagnostics.join("\n"));
             panic!("FAILED");
           }
-
-          output_text.push_str(&module_output_text);
         }
         let get_symbol_text =
           |module_symbol: deno_graph::symbols::ModuleInfoRef,
