4f151093b947c56bc53d3a95add1d821c58ce9d1
John Spurlock <47259736+johnspurlock-skymethod@users.noreply.github.com>
2024-02-06 08:52:19 -0600

    Fix 'explit' typo in hints.

--- src/fast_check/mod.rs
+++ src/fast_check/mod.rs
@@ -162,10 +162,10 @@ impl FastCheckDiagnostic {
         "fix the reference to point to a symbol that exists"
       }
       MissingExplicitType { .. } => {
-        "add an explit type annotation to the symbol"
+        "add an explicit type annotation to the symbol"
       }
       MissingExplicitReturnType { .. } => {
-        "add an explit return type to the function"
+        "add an explicit return type to the function"
       }
       UnsupportedAmbientModule { .. } => {
         "remove the ambient module declaration"
