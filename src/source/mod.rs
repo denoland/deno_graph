18da6a00ce6cbaa2888f76a7e96262ae12dc4d39
David Sherret <dsherret@gmail.com>
2024-09-24 21:25:00 -0400

    feat: include if root was dynamic in load options

--- src/source/mod.rs
+++ src/source/mod.rs
@@ -248,6 +248,8 @@ impl Locker for HashMapLocker {
 #[derive(Debug, Clone)]
 pub struct LoadOptions {
   pub is_dynamic: bool,
+  /// If the root specifier building the graph was in a dynamic branch.
+  pub was_dynamic_root: bool,
   pub cache_setting: CacheSetting,
   /// It is the loader's responsibility to verify the provided checksum if it
   /// exists because in the CLI we only verify the checksum of the source when
