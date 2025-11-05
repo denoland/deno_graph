0be4c5788304196c430f1beb04cb8b1de65ad193
Nayeem Rahman <nayeemrmn99@gmail.com>
2025-11-05 23:03:06 +0000

    check media type in RootSymbol::analyze_json_module()

--- src/symbols/analyzer.rs
+++ src/symbols/analyzer.rs
@@ -96,7 +96,7 @@ impl<'a> RootSymbol<'a> {
         })
         .or_else(|| self.analyze_js_module(js_module)),
       crate::Module::Independent(json_module) => {
-        Some(self.analyze_json_module(json_module))
+        self.analyze_json_module(json_module)
       }
       crate::Module::Wasm(wasm_module) => self.analyze_wasm_module(wasm_module),
       crate::Module::Npm(_)
@@ -177,7 +177,11 @@ impl<'a> RootSymbol<'a> {
   fn analyze_json_module(
     &self,
     json_module: &IndependentModule,
-  ) -> ModuleInfoRef<'_> {
+  ) -> Option<ModuleInfoRef<'_>> {
+    match json_module.media_type {
+      MediaType::Json | MediaType::Jsonc | MediaType::Json5 => {}
+      _ => return None,
+    }
     let specifier = &json_module.specifier;
     // it's not ideal having to use SourceTextInfo here, but it makes
     // it easier to interop with ParsedSource
@@ -222,7 +226,7 @@ impl<'a> RootSymbol<'a> {
       },
       source_text_info,
     };
-    self.finalize_insert(ModuleInfo::Json(Box::new(module_symbol)))
+    Some(self.finalize_insert(ModuleInfo::Json(Box::new(module_symbol))))
   }
 
   fn analyze_wasm_module(
