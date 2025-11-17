ae0d6ca4baf4801d87b84fac4c9510d1a21c682a
Nayeem Rahman <nayeemrmn99@gmail.com>
2025-11-17 06:50:08 +0000

    fix

--- tests/ecosystem_test.rs
+++ tests/ecosystem_test.rs
@@ -298,6 +298,7 @@ async fn test_version(
         skip_dynamic_deps: false,
         unstable_bytes_imports: false,
         unstable_text_imports: false,
+        unstable_extended_json_imports: false,
         module_analyzer: &module_analyzer,
         module_info_cacher: Default::default(),
         file_system: &NullFileSystem,
--- tests/helpers/mod.rs
+++ tests/helpers/mod.rs
@@ -163,6 +163,7 @@ pub struct TestBuilder {
   workspace_fast_check: bool,
   unstable_bytes_imports: bool,
   unstable_text_imports: bool,
+  unstable_extended_json_imports: bool,
 }
 
 impl TestBuilder {
@@ -182,6 +183,7 @@ impl TestBuilder {
       workspace_fast_check: false,
       unstable_bytes_imports: false,
       unstable_text_imports: false,
+      unstable_extended_json_imports: false,
     }
   }
 
@@ -272,6 +274,12 @@ impl TestBuilder {
   }
 
   #[allow(unused)]
+  pub fn unstable_extended_json_imports(&mut self, value: bool) -> &mut Self {
+    self.unstable_extended_json_imports = value;
+    self
+  }
+
+  #[allow(unused)]
   pub fn ensure_locker(&mut self) -> &mut Self {
     self.locker.get_or_insert_with(Default::default);
     self
@@ -333,6 +341,7 @@ impl TestBuilder {
           skip_dynamic_deps: self.skip_dynamic_deps,
           unstable_bytes_imports: self.unstable_bytes_imports,
           unstable_text_imports: self.unstable_text_imports,
+          unstable_extended_json_imports: self.unstable_extended_json_imports,
           jsr_version_resolver: Cow::Owned(JsrVersionResolver {
             newest_dependency_date_options: self.newest_dependency_date.clone(),
           }),
